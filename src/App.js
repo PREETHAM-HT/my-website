ea93c730d09dbe7207f04fc380f3068aa5ca3360
Preetham <itzpreetham010@gmail.com>
2024-12-05 13:56:07 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import Footer from './Footer';  // Import the Footer component
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My React Website</h1>
+        <p>This is a simple website built with React!</p>
       </header>
+      <Footer />  {/* Include the Footer component */}
     </div>
   );
 }
